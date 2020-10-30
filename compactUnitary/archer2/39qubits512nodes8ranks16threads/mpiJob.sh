5cf5adba2eb74d6b6bb9daac87c105089ab457f4
aniabrown <ania.brown@gmail.com>
2020-10-30 14:13:28 +0000

    adding archer2 timing for compactUnitary code

---
+++ compactUnitary/archer2/39qubits512nodes8ranks16threads/mpiJob.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#SBATCH --job-name=QuEST
+#SBATCH --time=1:00:0
+#SBATCH --nodes=512
+#SBATCH --tasks-per-node=8
+#SBATCH --cpus-per-task=16
+
+#SBATCH --account=y18
+#SBATCH --partition=standard
+#SBATCH --export=none
+#SBATCH --qos=standard
+
+module load epcc-job-env
+module restore PrgEnv-gnu
+#module restore /etc/cray-pe.d/PrgEnv-gnu
+
+export OMP_NUM_THREADS=16
+export OMP_PLACES=cores
+
+CMAKE_OPTIONS="-DUSER_SOURCE='compactUnitaryTimer.c' -DQuEST_DIR=QuEST_v2.1.0-gcc10Patch -DDISTRIBUTED=1 -DTESTING=0"
+
+rm -r build
+mkdir build; cd build
+cmake $CMAKE_OPTIONS ../../..
+make
+
+NUM_QUBITS=39
+NUM_TRIALS=50
+EXE=demo
+
+srun --hint=nomultithread --distribution=block:block ./$EXE $NUM_QUBITS $NUM_TRIALS
+
+cp TIMING* ..
+
---
+++ compactUnitary/archer2/singleNode/32qubits1nodes2ranks64threads/mpiJob.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#SBATCH --job-name=QuEST
+#SBATCH --time=0:30:0
+#SBATCH --nodes=1
+#SBATCH --tasks-per-node=2
+#SBATCH --cpus-per-task=64
+
+#SBATCH --account=y18
+#SBATCH --partition=standard
+#SBATCH --export=none
+#SBATCH --qos=standard
+
+module load epcc-job-env
+module restore PrgEnv-gnu
+#module restore /etc/cray-pe.d/PrgEnv-gnu
+
+export OMP_NUM_THREADS=64
+export OMP_PLACES=cores
+
+CMAKE_OPTIONS="-DUSER_SOURCE='compactUnitaryTimer.c' -DQuEST_DIR=QuEST_v2.1.0-gcc10Patch -DDISTRIBUTED=1 -DTESTING=0"
+
+rm -r build
+mkdir build; cd build
+cmake $CMAKE_OPTIONS ../../../..
+make
+
+NUM_QUBITS=32
+NUM_TRIALS=50
+EXE=demo
+
+srun --hint=nomultithread --distribution=block:block ./$EXE $NUM_QUBITS $NUM_TRIALS
+
+cp TIMING* ..
+
