26df766ca403da99f53e34b293df885e8b1fcace
aniabrown <ania.brown@gmail.com>
2020-10-30 14:11:17 +0000

    archer2 timing for random circuit

---
+++ randomCircuit/archer2/30qubits16nodes1ranks128threads/mpiJob.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#SBATCH --job-name=QuEST
+#SBATCH --time=2:00:0
+#SBATCH --nodes=16
+#SBATCH --tasks-per-node=1
+#SBATCH --cpus-per-task=128
+
+#SBATCH --account=y18
+#SBATCH --partition=standard
+#SBATCH --export=none
+#SBATCH --qos=standard
+
+module load epcc-job-env
+module restore PrgEnv-gnu
+#module restore /etc/cray-pe.d/PrgEnv-gnu
+
+export OMP_NUM_THREADS=128
+export OMP_PLACES=cores
+
+CMAKE_OPTIONS="-DUSER_SOURCE='RandomCircuit.c;RandomCircuitTimer.c' -DQuEST_DIR=QuEST_v2.1.0-gcc10Patch -DDISTRIBUTED=1 -DTESTING=0"
+
+rm -r build
+mkdir build; cd build
+cmake $CMAKE_OPTIONS ../../..
+make
+
+NUM_QUBITS=30
+NUM_TRIALS=50
+EXE=demo
+
+srun --hint=nomultithread --distribution=block:block ./$EXE $NUM_QUBITS $NUM_TRIALS
+
+cp TIMING* ..
+
